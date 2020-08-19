5f5076a5811a90ad6cf1d733a56eedcdae667752
egoljos <joshua.goldman@ericsson.com>
2020-08-19 21:51:03 +0200

    Added page for criteria 1/-84 info parsing

---
+++ src/Criteria_1_84/View.fs
@@ -0,0 +1,11 @@
+module Criteria_1_84.View
+
+open Elmish
+open Elmish.Navigation
+open Elmish.UrlParser
+open Fable.Core.JsInterop
+open Feliz
+open Feliz.style
+    
+let root model dispatch =
+    Html.none
